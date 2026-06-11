288f0e71244483e8e06053879efc363421dc35f2
anand10dds <anand@10dds.in>
2026-06-11 18:51:56 +0530

    updated tha latest image

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            New Version of the kubernetes deployment v3
+            New Version of the kubernetes deployment latest
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
