8fe7402497aaa0e2c688302660f31502ad5be8c7
anand10dds <anand@10dds.in>
2026-06-11 19:13:32 +0530

    updated the version 5

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            New Version of the kubernetes deployment latest
+            New Version of the kubernetes deployment v5
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
